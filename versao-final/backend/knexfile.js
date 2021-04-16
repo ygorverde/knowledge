2c6496d0ecd71bea00d21f918ce071d0508495aa
ygorverde <ygorverde572@gmail.com>
2021-04-16 14:00:36 -0300

    .

--- versao-final/backend/knexfile.js
+++ versao-final/backend/knexfile.js
@@ -1,8 +1,23 @@
-const { db } = require('./.env')
+// const { db } = require('./.env')
 
+// module.exports = {
+// 	client: 'postgresql',
+// 	connection: db,
+// 	pool: {
+// 		min: 2,
+// 		max: 10
+// 	},
+// 	migrations: {
+// 		tableName: 'knex_migrations'
+// 	}
+// };
 module.exports = {
 	client: 'postgresql',
-	connection: db,
+	connection: {
+		database: 'knowledge_final',
+		user: 'postgres',
+		password: '123456'
+	},
 	pool: {
 		min: 2,
 		max: 10
@@ -10,4 +25,4 @@ module.exports = {
 	migrations: {
 		tableName: 'knex_migrations'
 	}
-};
+}
